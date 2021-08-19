64c36f5967452d23e6529f41c82ead9c2032f18a
Eduardo Bellido Bellido <edubxb@gmail.com>
2021-08-19 23:07:29 +0200

    Improve start timer logic

--- js/main.js
+++ js/main.js
@@ -2,13 +2,26 @@ document.addEventListener('DOMContentLoaded', () => {
     var startDate = new Date("Aug 23, 2021 00:00");
     var returnDate = new Date("Sep 13, 2021 08:00");
 
-    if (startDate <= Date.now()) {
+    if (startDate <= Date.now() && returnDate >= Date.now()) {
+        Array.prototype.map.call(document.getElementsByClassName('here'),
+            function(element) {
+                element.style.display = 'none';
+            }
+        );
+        Array.prototype.map.call(document.getElementsByClassName('away'),
+            function(element) {
+                element.style.display = 'block';
+            }
+        );
+
+        console.log('The countdown has started!');
         var flipdown = new FlipDown(
             returnDate.getTime() / 1000,
             { theme: "dark" }
         )
         .start()
         .ifEnded(() => {
+            console.log('The countdown has ended!');
             Array.prototype.map.call(document.getElementsByClassName('here'),
                 function(element) {
                 element.style.display = 'block';
@@ -19,18 +32,18 @@ document.addEventListener('DOMContentLoaded', () => {
                 element.style.display = 'none';
                 }
             );
-            console.log('The countdown has ended!');
         });
+    }
+    else {
         Array.prototype.map.call(document.getElementsByClassName('here'),
             function(element) {
-               element.style.display = 'none';
+                element.style.display = 'block';
             }
         );
         Array.prototype.map.call(document.getElementsByClassName('away'),
             function(element) {
-               element.style.display = 'block';
+                element.style.display = 'none';
             }
         );
-        console.log('The countdown has started!');
     };
 });
