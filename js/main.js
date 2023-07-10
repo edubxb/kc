c448759540964154b97b85e11ed9202c7c99b268
Eduardo Bellido Bellido <edubxb@gmail.com>
2023-07-10 21:57:49 +0200

    Summer holidays!!!

--- js/main.js
+++ js/main.js
@@ -36,8 +36,8 @@ function startCountdown(returnDate) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    let startDate = new Date("Apr 6, 2023 13:00");
-    let returnDate = new Date("Apr 12, 2023 08:00");
+    let startDate = new Date("Jul 07, 2023 15:00");
+    let returnDate = new Date("Jul 24, 2023 08:00");
 
     currentDate = new Date();
     isEduHere(true);
